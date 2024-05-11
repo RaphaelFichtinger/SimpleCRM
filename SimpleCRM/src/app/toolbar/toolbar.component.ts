0f05d29d2c943121eb06f2ce4d35ee11e3021bc5
Raphael Fichtinger <153192560+RaphaelFichtinger@users.noreply.github.com>
2024-05-11 17:42:09 +0200

    added clock to toolbar

--- SimpleCRM/src/app/toolbar/toolbar.component.ts
+++ SimpleCRM/src/app/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { UserComponent } from '../user/user.component';
-
+import { Subscription } from 'rxjs';
+import { CommonModule } from "@angular/common"; 
 
 @Component({
   selector: 'app-toolbar',
@@ -18,11 +19,28 @@ import { UserComponent } from '../user/user.component';
     RouterLink,
     RouterLinkActive,
     DashboardComponent,
-    UserComponent
+    UserComponent,
+    CommonModule
       ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit, OnDestroy {
 
+  time = new Date();
+  intervalId:any;
+  subscription: Subscription|any;
+
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
